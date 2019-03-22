21a720d0042327af97d16196353798a9d6a12cac
asosori <tsukimiteritama@gmail.com>
2019-03-23 01:49:25 +0900

    implemented a user function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def inquire
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :correct_user, only: [:show]
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      flash[:danger] = "権限がありません"
+      redirect_to root_url
+    end
+  end
+
+  def show
+  end
+
+  
+end
