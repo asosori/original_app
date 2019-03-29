8ac674cef8b26a9d4906a75ef5fabc7e7723ff23
asosori <tsukimiteritama@gmail.com>
2019-03-29 18:05:55 +0900

    modified a migration file

--- db/migrate/20190324225437_add_index_posts_professor.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexPostsProfessor < ActiveRecord::Migration[5.2]
-  def change
-    add_index :posts, :professor
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_050919) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["professor"], name: "index_posts_on_professor"
     t.index ["university", "department", "major", "professor"], name: "add_index_to_posts"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
