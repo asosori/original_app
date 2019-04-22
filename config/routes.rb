54971430010667ce70cdd432de1a97cdcf088d17
asosori <tsukimiteritama@gmail.com>
2019-04-23 01:42:24 +0900

    implemented an active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   post "likes/:post_id" => "likes#create"
   delete "likes/:post_id" => "likes#destroy"
 
