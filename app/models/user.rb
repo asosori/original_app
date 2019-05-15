fdc901a8c40884725ed1b115d1cb14b585e0e67c
asosori <tsukimiteritama@gmail.com>
2019-05-16 01:51:25 +0900

    an experiment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
   has_one_attached :image
-  attribute :new_image
-
-  before_save do
-    self.image = new_image if new_image
-  end
+  
 
   devise :database_authenticatable, 
         :registerable,
